d42eb6a6a8a318b7d5d17736bb316e5372397abc
Kavishka Sasindu <sasindu0215@gmail.com>
2026-06-30 16:53:56 +0530

    updated test file

--- batfish/batfish_test.py
+++ batfish/batfish_test.py
@@ -5,7 +5,6 @@ bf = Session(host="172.23.71.245", port="9996")
 try:
     bf.list_networks()
     print("Hey from Batfish Server.....")
-    print("Connection is established....")
     print("✔ Batfish reachable")
 
 except Exception as e:

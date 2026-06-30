2c90ee73b757fa345e7f37ad8a36e17cd38dbd65
Kavishka Sasindu <sasindu0215@gmail.com>
2026-06-30 16:47:25 +0530

    updated test python file

--- batfish/batfish_test.py
+++ batfish/batfish_test.py
@@ -4,8 +4,12 @@ bf = Session(host="172.23.71.245",port="9996")
 
 try:
     bf.list_networks()
+<<<<<<< HEAD
     print("Hey from Batfish Server....wq
 ")
+=======
+    print("Hey from Batfish Server.....")
+>>>>>>> 70ad60b (updated test python file)
     print("✔ Batfish reachable")
 except Exception as e:
     # Error Logging

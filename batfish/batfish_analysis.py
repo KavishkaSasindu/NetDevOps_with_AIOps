3caf2312bef866176b3875224d448519c53adb97
Kavishka Sasindu <sasindu0215@gmail.com>
2026-07-01 14:51:06 +0530

    updated batfish analyser

--- batfish/batfish_analysis.py
+++ batfish/batfish_analysis.py
@@ -74,6 +74,8 @@ try:
         pathConstraints=PathConstraints(
             startLocation="h1",
         ),
+        headers=HeaderConstraints(
+            dstIps="192.168.1.0/24, 192.168.2.0/24, 192.168.3.0/24"
     ).answer()
     reach = reach_answer.frame()
 
